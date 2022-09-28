bba3db41072760f0c9c78a76d62fd22de11709d7
williameiji <william.fujimori@gmail.com>
2022-09-28 13:30:14 -0300

    fix: change students types to correct file

--- src/infra/utils/types.ts
+++ src/infra/utils/types.ts
@@ -13,3 +13,23 @@ export type TAuth = Omit<Auth, "id">;
 export type TLogin = Omit<Auth, "id" | "key">;
 
 export type TUser = Omit<Auth, "key">;
+
+export interface Student extends WithId<Document> {
+	id: ObjectId;
+	date: string;
+	value: string;
+	name: string;
+	cpfStudent: string;
+	rgStudent: string;
+	nameResp: string;
+	cpfResp: string;
+	rgResp: string;
+	adress: string;
+	number: string;
+	district: string;
+	city: string;
+	phone: string;
+	email: string;
+}
+
+export type TStudent = Partial<Student>;
--- src/repositories/studentRepository.ts
+++ src/repositories/studentRepository.ts
@@ -1,8 +1,7 @@
 import { db } from "../databases/mongo";
-import { ObjectId } from "mongodb";
-import type { WithId, Document } from "mongodb";
+import * as types from "../infra//utils/types";
 
-export async function insert(data: TStudent) {
+export async function insert(data: types.TStudent) {
 	await db.students.insertOne(data);
 }
 
@@ -10,11 +9,11 @@ export async function findStudentByPartialName(name: string) {
 	return await db.students.find({ name: new RegExp(name, "i") }).toArray();
 }
 
-export async function findByName(name: string): Promise<Student> {
-	return (await db.students.findOne({ name })) as Student;
+export async function findByName(name: string): Promise<types.Student> {
+	return (await db.students.findOne({ name })) as types.Student;
 }
 
-export async function edit(student: TStudent, data: TStudent) {
+export async function edit(student: types.TStudent, data: types.TStudent) {
 	await db.students.updateOne(student, {
 		$set: {
 			date: data.date,
@@ -34,23 +33,3 @@ export async function edit(student: TStudent, data: TStudent) {
 		},
 	});
 }
-
-export interface Student extends WithId<Document> {
-	id: ObjectId;
-	date: string;
-	value: string;
-	name: string;
-	cpfStudent: string;
-	rgStudent: string;
-	nameResp: string;
-	cpfResp: string;
-	rgResp: string;
-	adress: string;
-	number: string;
-	district: string;
-	city: string;
-	phone: string;
-	email: string;
-}
-
-export type TStudent = Partial<Student>;
