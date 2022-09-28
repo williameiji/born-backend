88a33033d02a4829a1f271b7200bf9e48a653c25
williameiji <william.fujimori@gmail.com>
2022-09-28 12:58:06 -0300

    feat: add validate schema on signup route

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -3,10 +3,11 @@ import { Router } from "express";
 import * as authController from "../controllers/authController";
 import { validateSchema } from "../infra/middleware/schemasValidator";
 import loginSchema from "../infra/schemas/loginSchema";
+import signupSchema from "../infra/schemas/signupSchema";
 
 const authRouter = Router();
 
 authRouter.post("/login", validateSchema(loginSchema), authController.login);
-authRouter.post("/signup", authController.signup);
+authRouter.post("/signup", validateSchema(signupSchema), authController.signup);
 
 export default authRouter;
