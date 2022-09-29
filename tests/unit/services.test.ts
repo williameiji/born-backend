ed58d143763c34d91dc97f52a1d2f49d73d65df5
williameiji <william.fujimori@gmail.com>
2022-09-29 12:07:48 -0300

    test: unit insert user with valid params

--- tests/unit/services.test.ts
+++ tests/unit/services.test.ts
@@ -1,5 +1,31 @@
+import * as authService from "../../src/services/authService";
+import * as authRepository from "../../src/repositories/authRepository";
+import { userFactory } from "../factories/userFactory";
+
+beforeEach(async () => {
+	jest.resetAllMocks();
+	jest.clearAllMocks();
+});
+
 describe("Auth test", () => {
-	it.todo("Test signup with valid params");
+	it("Test signup with valid params", async () => {
+		const findUser = jest
+			.spyOn(authRepository, "findUserByName")
+			.mockResolvedValueOnce(null);
+
+		const insertUser = jest
+			.spyOn(authRepository, "insert")
+			.mockImplementation(async () => {
+				return null;
+			});
+
+		const user = await userFactory();
+
+		await authService.signup(user);
+
+		expect(findUser).toBeCalled();
+		expect(insertUser).toBeCalled();
+	});
 
 	it.todo("Test signup with invalid company key");
 
