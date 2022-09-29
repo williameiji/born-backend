9c34f9571d1767001e5bbb384632e51ea4e01f4c
williameiji <william.fujimori@gmail.com>
2022-09-29 09:07:36 -0300

    feat: add todo tests on auth route

--- tests/integration/services.test.ts
+++ tests/integration/services.test.ts
@@ -1,3 +1,15 @@
 describe("Auth test", () => {
-	it.todo("Test login with valid params", async () => {});
+	it.todo("Test signup with valid params");
+
+	it.todo("Test signup with invalid company key");
+
+	it.todo("Test signup with invalid username");
+
+	it.todo("Test signup with invalid password");
+
+	it.todo("Test login with valid params");
+
+	it.todo("Test login with invalid username");
+
+	it.todo("Test login with invalid password");
 });
