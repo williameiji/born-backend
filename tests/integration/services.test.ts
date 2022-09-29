a19b9a0f260919beb483256207b33a58d47d32f9
williameiji <william.fujimori@gmail.com>
2022-09-29 10:52:22 -0300

    test: signup route with valid params

--- tests/integration/services.test.ts
+++ tests/integration/services.test.ts
@@ -1,5 +1,31 @@
+import supertest from "supertest";
+import app from "../../src/index";
+import { userFactory } from "../factories/userFactory";
+import { db, mongoClient, connectToDatabase } from "../../src/databases/mongo";
+
+const server = supertest(app);
+
+beforeAll(async () => {
+	await connectToDatabase();
+});
+
+afterAll(async () => {
+	await mongoClient.close();
+});
+
 describe("Auth test", () => {
-	it.todo("Test signup with valid params");
+	it("Test signup with valid params", async () => {
+		const user = await userFactory();
+
+		const result = await server.post("/signup").send(user);
+
+		const isCreated = await db.users.findOne({
+			name: user.name,
+		});
+
+		expect(result.status).toBe(201);
+		expect(isCreated).not.toBeNull();
+	});
 
 	it.todo("Test signup with invalid company key");
 
