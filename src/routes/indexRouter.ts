7b662c220ee8a8756e380e54d8791eb2bb1f52bd
williameiji <william.fujimori@gmail.com>
2022-10-05 12:05:19 -0300

    feat: add route for e2e tests

--- src/routes/indexRouter.ts
+++ src/routes/indexRouter.ts
@@ -1,11 +1,19 @@
 import { Router } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 import authRouter from "./authRouter";
 import studentsRouter from "./studentsRouter";
+import e2eRouter from "./e2eRouter";
 
 const router = Router();
 
 router.use(authRouter);
 router.use(studentsRouter);
+
+if (process.env.MODE === "DEV") {
+	router.use(e2eRouter);
+}
 
 export default router;
