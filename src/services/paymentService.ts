0895e1bf024a5d468febb810f37e19b34f136551
williameiji <william.fujimori@gmail.com>
2022-10-06 13:38:25 -0300

    feat: Function to return payments

--- src/services/paymentService.ts
+++ src/services/paymentService.ts
@@ -3,3 +3,7 @@ import * as paymentRepository from "../repositories/paymentRepository";
 export async function addPayment(data) {
 	await paymentRepository.insert(data);
 }
+
+export async function sendPayments(id: string) {
+	return paymentRepository.getPayments(id);
+}
