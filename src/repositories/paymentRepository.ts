74da96bb718996239ead64af80e3d6219780c80e
williameiji <william.fujimori@gmail.com>
2022-10-06 13:37:19 -0300

    feat: get payments by id from database

--- src/repositories/paymentRepository.ts
+++ src/repositories/paymentRepository.ts
@@ -3,3 +3,7 @@ import { db } from "../databases/mongo";
 export async function insert(data) {
 	await db.payments.insertOne(data);
 }
+
+export async function getPayments(id: string) {
+	return await db.payments.find({ id }).toArray();
+}
