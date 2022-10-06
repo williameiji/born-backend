3b7ca89a26d1916546771e58b6bb4d6542780356
williameiji <william.fujimori@gmail.com>
2022-10-06 13:39:11 -0300

    feat: function to send payments by id

--- src/controllers/paymentController.ts
+++ src/controllers/paymentController.ts
@@ -8,3 +8,11 @@ export async function addPayment(req: Request, res: Response) {
 
 	res.sendStatus(201);
 }
+
+export async function sendPayments(req: Request, res: Response) {
+	const { id } = req.params;
+
+	const payments = await paymentService.sendPayments(id);
+
+	res.status(200).send(payments);
+}
