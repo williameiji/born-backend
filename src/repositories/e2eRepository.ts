8b0c246bf6dfb282fce45f6c02c4dbc2d37f7169
williameiji <william.fujimori@gmail.com>
2022-10-08 09:59:24 -0300

    feat: add clearpayment collection

--- src/repositories/e2eRepository.ts
+++ src/repositories/e2eRepository.ts
@@ -3,4 +3,5 @@ import { db } from "../databases/mongo";
 export async function clearDatabase() {
 	await db.students.deleteMany({});
 	await db.users.deleteMany({});
+	await db.payments.deleteMany({});
 }
