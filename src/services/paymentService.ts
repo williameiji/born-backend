80a82bed2c2d8b572eed4c4bf3f787678e57c29b
williameiji <william.fujimori@gmail.com>
2022-10-08 10:00:09 -0300

    feat: add check if user exists

--- src/services/paymentService.ts
+++ src/services/paymentService.ts
@@ -1,10 +1,15 @@
 import * as paymentRepository from "../repositories/paymentRepository";
+import * as studentService from "../services/studentService";
 import * as types from "../infra/utils/types";
 
 export async function addPayment(data: types.TPayments) {
+	await studentService.checkIfStudentExist(data.id);
+
 	await paymentRepository.insert(data);
 }
 
 export async function sendPayments(id: string) {
+	await studentService.checkIfStudentExist(id);
+
 	return paymentRepository.getPayments(id);
 }
