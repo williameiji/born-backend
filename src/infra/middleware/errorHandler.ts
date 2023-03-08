56e9212f4777d671a76fffadccc4804c46a79155
William <william.fujimori@gmail.com>
2023-03-08 11:08:42 -0300

    fix: remove error 500

--- src/infra/middleware/errorHandler.ts
+++ src/infra/middleware/errorHandler.ts
@@ -8,9 +8,8 @@ export default async function errorHandler(
 ) {
 	if (error.code === "BadRequest") return res.status(400).send(error.message);
 	if (error.code === "WrongType") return res.status(422).send(error.message);
-	if (error.code === "Anauthorized") return res.status(401).send(error.message);
+	if (error.code === "Anauthorized")
+		return res.status(401).send(error.message);
 	if (error.code === "NotFound") return res.status(404).send(error.message);
 	if (error.code === "Conflict") return res.status(409).send(error.message);
-
-	return res.sendStatus(500);
 }
