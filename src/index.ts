691c9fb98f3d88cde4f5177c1b77914fe84ff563
William <william.fujimori@gmail.com>
2023-03-09 10:08:45 -0300

    feat: add cors options

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,25 @@ import "express-async-errors";
 import cors from "cors";
 import router from "./routes/indexRouter";
 import errorHandler from "./infra/middleware/errorHandler";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
+let origin = "https://born-frontend.vercel.app";
+
+if (process.env.MODE === "DEV") {
+	origin = "http://localhost:3000";
+}
+
+const corsOptions = {
+	origin,
+	optionsSuccessStatus: 200,
+};
+
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(router);
 app.use(errorHandler);
 
