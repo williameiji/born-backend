d83320972ffe9a682ff571bfb2bc18cab61716b3
William Eiji Fujimori <william.fujimori@gmail.com>
2026-07-26 13:32:03 -0300

    Change findUserByName to use email for user lookup
    Updated findUserByName function to query by email instead of name.

--- src/repositories/authRepository.ts
+++ src/repositories/authRepository.ts
@@ -2,9 +2,7 @@ import { db } from "../databases/mongo";
 import * as types from "../infra/utils/types";
 
 export async function findUserByName(name: string): Promise<types.TUser> {
-	return await db.users.findOne({
-		name,
-	});
+	return (await db.users.findOne({ email })) as types.TUser;
 }
 
 export async function insert(data: types.TAuth) {
