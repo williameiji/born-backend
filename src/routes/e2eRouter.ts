88bfae3d8b049539dddeb11fac02c4a19a8d0c88
William Eiji Fujimori <william.fujimori@gmail.com>
2026-07-26 13:14:43 -0300

    Delete src/routes/e2eRouter.ts

--- src/routes/e2eRouter.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import * as e2eController from "../controllers/e2eController";
-
-const e2eRouter = Router();
-
-e2eRouter.post("/e2e/cleardb", e2eController.clearDatabase);
-
-export default e2eRouter;
