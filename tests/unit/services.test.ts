a371c8300203ba84b0305c98297a7a03fbe86542
William Eiji Fujimori <william.fujimori@gmail.com>
2026-07-26 11:26:45 -0300

    Fix typo in error code from 'Anauthorized' to 'Unauthorized'

--- tests/unit/services.test.ts
+++ tests/unit/services.test.ts
@@ -64,7 +64,7 @@ describe("Auth test", () => {
 
 		expect(findUser).toBeCalled();
 		expect(error).rejects.toEqual({
-			code: "Anauthorized",
+			code: "Unauthorized",
 			message: "Chave da empresa incorreta",
 		});
 	});
@@ -109,7 +109,7 @@ describe("Auth test", () => {
 
 		expect(findUser).toBeCalled();
 		expect(error).rejects.toEqual({
-			code: "Anauthorized",
+			code: "Unauthorized",
 			message: "Login/Senha incorretos",
 		});
 	});
