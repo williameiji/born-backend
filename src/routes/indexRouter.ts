991ed545a364e528685bc5c8b65df4ed324f376e
William Eiji Fujimori <william.fujimori@gmail.com>
2026-07-26 13:21:32 -0300

    Remove e2eRouter from indexRouter
    Removed e2eRouter import and conditional usage.

--- src/routes/indexRouter.ts
+++ src/routes/indexRouter.ts
@@ -5,7 +5,6 @@ dotenv.config();
 
 import authRouter from "./authRouter";
 import studentsRouter from "./studentsRouter";
-import e2eRouter from "./e2eRouter";
 import paymentRouter from "./paymentRouter";
 
 const router = Router();
@@ -13,9 +12,5 @@ const router = Router();
 router.use(authRouter);
 router.use(studentsRouter);
 router.use(paymentRouter);
-
-if (process.env.MODE === "DEV") {
-	router.use(e2eRouter);
-}
 
 export default router;
