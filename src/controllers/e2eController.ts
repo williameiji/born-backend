18b917ba40a78e77ed72dc66dd8970a421411d5c
William Eiji Fujimori <william.fujimori@gmail.com>
2026-07-26 13:14:10 -0300

    Delete src/controllers/e2eController.ts

--- src/controllers/e2eController.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express";
-import * as e2eService from "../services/e2eService";
-
-export async function clearDatabase(req: Request, res: Response) {
-	await e2eService.clearDatabase();
-
-	res.sendStatus(202);
-}
